e1d5e298d9b76a5eec71aef5fe291a5a22ed237f
rohjuhui <hoe0124@gmail.com>
2023-02-16 23:42:33 +0900

    Feat: 11050_이항계수1 수정 log 지움

--- beakjoon/class2/11050_이항계수1.js
+++ beakjoon/class2/11050_이항계수1.js
@@ -2,9 +2,7 @@ let inputs = require('fs').readFileSync('../example.txt').toString().trim().spli
 // let inputs = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
 
 let n = Number(inputs[0]);
-console.log('n: ', n);
 let k = Number(inputs[1]);
-console.log('k: ', k);
 
 let n_factorial = 1;
 let k_factorial = 1;
@@ -19,8 +17,5 @@ for(let j=1; j<=k; j++){
 for(let z=1; z<=(n-k); z++){
 	n_k_factorial *= z;
 }
-console.log('n_factorial: ', n_factorial); 
-console.log('k_factorial: ', k_factorial);
-console.log('n_k_factorial: ', n_k_factorial);
 
 console.log(n_factorial/(k_factorial*n_k_factorial));
