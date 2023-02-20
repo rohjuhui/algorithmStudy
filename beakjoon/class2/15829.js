2c4159bf44914746f87802c1f2453cc28fac8898
rohjuhui <117527055+rohjuhui@users.noreply.github.com>
2023-02-20 23:27:59 +0900

    fix: fix error for 15829 hashing

--- beakjoon/class2/15829.js
+++ beakjoon/class2/15829.js
@@ -1,13 +1,13 @@
 let [l, str] = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
 // let [l, str] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-let r = 31;
+let r = 1;
 let m = 1234567891;
 
 let hash = 0;
-for(let i=0; i<l; i++){
-	
-	// 영어 소문자 아스키코드 a-z > 97-122
-	hash += ((str.charCodeAt(i) - 96) * Math.pow(r, i)) % m;
+for (let i = 0; i < l; i++) {
+hash += (str.charCodeAt(i) - 96) * r;
+
+r =(r* 31)% m ;
 }
-console.log(hash);
+console.log(hash %m);
