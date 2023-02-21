81ed856eeda3ddb20aecce16f186de20298f74e8
rohjuhui <hoe0124@gmail.com>
2023-02-21 22:21:04 +0900

    Feat: 11651 좌표정렬하기2

---
+++ beakjoon/class2/11651_좌표정렬하기2.js
@@ -0,0 +1,6 @@
+let [n, ...inputs] = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
+// let [n, ...inputs] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+inputs.sort( (a, b) => a.split(' ')[0] - b.split(' ')[0])
+.sort( (a, b) => a.split(' ')[1] - b.split(' ')[1]);
+console.log(inputs.join('\n'));
