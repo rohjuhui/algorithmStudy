ea616a875a9cca6ed707217c07e295abf1da9522
rohjuhui <hoe0124@gmail.com>
2023-03-01 19:00:16 +0900

    Feat: 10866 덱

---
+++ beakjoon/class2/10866_덱.js
@@ -0,0 +1,49 @@
+let [N, ...orders] = require('fs').readFileSync('../example.txt').toString().trim().split('\n');
+// let [N, ...orders] = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let queue = [];
+let result = [];
+for(let i=0; i<Number(N); i++){
+	let order = orders[i].toString().trim().split(' ')[0];
+	if(order === 'push_front'){
+		let num = Number(orders[i].toString().trim().split(' ')[1]);
+		queue.unshift(num);
+	}else if(order === 'push_back'){
+		let num = Number(orders[i].toString().trim().split(' ')[1]);
+		queue.push(num);
+	}else if(order === 'pop_front'){
+		if(queue.length == 0){
+			result.push(-1);
+		}else{
+			result.push(queue.shift());
+		}
+	}else if(order === 'pop_back'){
+		if(queue.length == 0){
+			result.push(-1);
+		}else{
+			result.push(queue.pop());
+		}
+	}else if(order === 'size'){
+		result.push(queue.length);
+	}else if(order === 'empty'){
+		if(queue.length == 0){
+			result.push(1);
+		}else{
+			result.push(0);
+		}
+	}else if(order === 'front'){
+		if(queue.length == 0){
+			result.push(-1);
+		}else{
+			result.push(queue[0]);
+		}
+	}else if(order === 'back'){
+		if(queue.length == 0){
+			result.push(-1);
+		}else{
+			result.push(queue[queue.length-1]);
+		}
+	}
+}
+
+console.log(result.join('\n'));
