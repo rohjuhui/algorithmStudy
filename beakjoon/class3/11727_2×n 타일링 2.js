efe3d2fd3cd03277a5500be3825db3688e56944e
rohjuhui <hoe0124@gmail.com>
2023-05-28 11:58:31 +0900

    feat: 11727번 문제 해결

---
+++ beakjoon/class3/11727_2×n 타일링 2.js
@@ -0,0 +1,12 @@
+let N = require('fs').readFileSync('../example.txt').toString().trim();
+// let N = require('fs').readFileSync('/dev/stdin').toString().trim();
+N = +N;
+
+let dp = Array.from(N+1).fill(0);
+dp[1] = 1;
+dp[2] = 3;
+
+for(let i=3; i<N+1; i++){
+    dp[i] = ((2*dp[i-2]) + dp[i-1])%10007;
+}
+console.log(dp[N]);
